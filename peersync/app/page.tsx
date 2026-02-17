612554f4413b1515e023024ae1552a6973a63d9b
AdityaVerma810 <adicode810@gmail.com>
2026-02-17 18:33:34 +0530

    Added Payment, Plans subscriptions feature in my application using Clerk billing feature

--- peersync/app/page.tsx
+++ peersync/app/page.tsx
@@ -1,9 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div >
       <h1>Heloooo Next.js</h1>
+      <PricingTable/>
     </div>
   );
 }
