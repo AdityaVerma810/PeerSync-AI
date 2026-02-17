5db10cf217fa0cdbd94421582dcb32c098b3725c
AdityaVerma810 <adicode810@gmail.com>
2026-02-18 00:21:14 +0530

    completed the Header section, added signIn, signOut features using clerk

--- peersync/app/layout.tsx
+++ peersync/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut,SignInButton,SignUpButton,UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/ui/layout/header";
+import HeaderWrapper from "@/components/ui/layout/header-wrapper"
 
 const outfitFont = Outfit({
   
@@ -27,24 +29,7 @@ export default function RootLayout({
       <body
         className={`${outfitFont.className} antialiased`}
       >
-         <header className="flex justify-end items-center p-4 gap-4 h-16">
-              
-              {/* Show the sign-in and sign-up buttons when the user is signed out */}
-              
-
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
-              {/* Show the user button when the user is signed in */}
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
+       <HeaderWrapper />
         {children}
       </body>
     </html>
