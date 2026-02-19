ede20ffee5a3c9ac01c7dffa7701c9c367f14e3a
AdityaVerma810 <adicode810@gmail.com>
2026-02-19 15:11:50 +0530

    Created the hero secion of my landing page

--- peersync/components/ui/landing/hero-section.tsx
+++ peersync/components/ui/landing/hero-section.tsx
@@ -1,5 +1,60 @@
+import { RocketIcon, SparkleIcon, ZapIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { HeroGradient } from "./background-gradient";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+
 export default function HeroSection() {
   return (
-    <div>hero</div>
+    <section className="relative overflow-hidden">
+      <HeroGradient />
+      <div className="relative section-container section-padding">
+        <div className="text-center">
+          <Badge className="mb-6 text-sm font-medium" variant="secondary">
+            Powered by AI <SparkleIcon className="size-4 inline-block ml-2" />
+          </Badge>
+          <h1>
+            Discover Your Perfect{" "}
+            <span className="block gradient-text">AI Learning Partner</span>
+          </h1>
+          <p className="hero-subheading">
+            Join communities, set your learning goals, and get matched with
+            partners who share your passion. Chat, collaborate, and grow
+            together with AI-powered insights.
+          </p>
+          <div className="flex flex-col gap-4 sm:flex-row sm:justify-center">
+            
+              <Link href="/sign-up">
+                <Button
+                  size="lg"
+                  className="link-button hero-button-outline group"
+                >
+                  <span className="hero-button-content">
+                    <RocketIcon className="hero-button-icon-outline group-hover:rotate-12 group-hover:text-primary" />
+                    <span className="hero-button-text">
+                      Get Started for Free
+                    </span>
+                  </span>
+                </Button>
+              </Link>
+            
+           
+              <Link href="/#pricing">
+                <Button
+                  size="lg"
+                  className="link-button hero-button-primary group"
+                >
+                  <span className="hero-button-content">
+                    <ZapIcon className="hero-button-icon-primary group-hover:scale-125 group-hover:rotate-12" />
+                    Buy a Plan
+                  </span>
+                </Button>
+              </Link>
+            
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
